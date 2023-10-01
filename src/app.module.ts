a0bece39e879d6a11efa1745537e77330061a3df
mztoja <mztoja@gmail.com>
2023-10-01 14:30:11 +0200

    First version of user authentication system

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ArticlesModule } from './articles/articles.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthModule, UsersModule, ArticlesModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { appConstants } from './constants';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3001);
+  app.setGlobalPrefix('v1');
+  app.enableCors();
+  await app.listen(appConstants.port);
+  console.log(`[bootstrap] server is running on port: ${appConstants.port}`)
 }
 bootstrap();
