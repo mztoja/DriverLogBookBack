4a5f5a6b2bc53772e43760fe68efa9469751117c
mztoja <mztoja@gmail.com>
2023-10-17 01:22:48 +0200

    Add mark departure in user service

--- src/types/user/index.ts
+++ src/types/user/index.ts
@@ -2,3 +2,4 @@ export * from './UserEnums';
 export * from './UserInterface';
 export * from './LoginFormInterface';
 export * from './RegisterFormInterface';
+export * from './MarkDepartInterface';
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,22 @@
-import { Controller, Inject } from '@nestjs/common';
-
+import { Body, Controller, Inject, Patch } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { JwtService } from '@nestjs/jwt';
 
 @Controller('users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
+
   @Inject(JwtService)
   private jwtService: JwtService;
+
+  @Patch('markDepart')
+  async markDepart(
+    @Body('userId') userId: string,
+    @Body('placeId') placeId: string,
+  ) {
+    await this.usersService.markDepart(userId, Number(placeId));
+    return {
+      message: 'departure place marked',
+    };
+  }
 }
