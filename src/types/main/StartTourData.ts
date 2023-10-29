cdefb5ce709ac18f78c2cc7b7790a6662e248099
mztoja <mztoja@gmail.com>
2023-10-29 20:06:49 +0100

    Updated tours and logs module after changes of the authorization method

---
+++ src/types/main/StartTourData.ts
@@ -0,0 +1,11 @@
+export interface StartTourData {
+  date: string;
+  action: string;
+  country: string;
+  place: string;
+  placeId: string;
+  odometer: string;
+  notes: string;
+  truck: string;
+  fuelStateBefore: string;
+}
