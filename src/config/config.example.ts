cd15599145cc4b9d48241fbe3074829dec54b653
mztoja <mztoja@gmail.com>
2024-07-10 15:34:20 +0200

    mail disable

--- src/config/config.example.ts
+++ src/config/config.example.ts
@@ -13,6 +13,7 @@
   mailerAuthPassword: 'password',
   mailerHost: 'localhost',
   mailerSmtpPort: 2500,
+  mailerFrom: 'App@DriverLogBook.com',
   secure: true, // if you use https then change it to TRUE !!!!
   domain: 'localhost',
   httpOnly: true,
