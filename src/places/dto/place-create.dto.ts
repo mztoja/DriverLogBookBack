7337fc2dd4306a1dea334ee92e1a2deda88da3b7
mztoja <mztoja@gmail.com>
2026-09-03 18:14:11 +0200

    fix validate input length in place edit/add forms

--- src/places/dto/place-create.dto.ts
+++ src/places/dto/place-create.dto.ts
@@ -1,5 +1,11 @@
 import { placeTypeEnum } from '../../types';
-import { IsBoolean, IsNotEmpty, IsNumber, IsString } from 'class-validator';
+import {
+  IsBoolean,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  MaxLength,
+} from 'class-validator';
 import { Transform } from 'class-transformer';
 
 export class PlaceCreateDto {
@@ -10,13 +16,17 @@ export class PlaceCreateDto {
   type: placeTypeEnum;
   @IsString()
   @IsNotEmpty({ message: 'name' })
+  @MaxLength(30, { message: 'placeNameTooLong' })
   name: string;
   @IsString()
+  @MaxLength(50, { message: 'placeStreetTooLong' })
   street: string;
   @IsString()
+  @MaxLength(10, { message: 'placeCodeTooLong' })
   code: string;
   @IsString()
   @IsNotEmpty({ message: 'city' })
+  @MaxLength(30, { message: 'placeCityTooLong' })
   city: string;
   @IsString()
   @IsNotEmpty({ message: 'country' })
